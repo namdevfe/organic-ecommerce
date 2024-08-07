b626238f893de03c252e8d8bc67713a29158acce
Nguyễn Kim Quốc Nam <namnkq2603@gmail.com>
2024-08-07 16:18:50 +0700

    fix(cart): change method post to put on add to cart feature

--- server/src/routes/v1/cartRoute.ts
+++ server/src/routes/v1/cartRoute.ts
@@ -6,6 +6,6 @@ import cartValidation from '~/validations/cartValidation'
 const router: Router = express.Router()
 
 router.get('/', verifyToken, cartController.getCart)
-router.post('/add-to-cart', [verifyToken, cartValidation.addToCart], cartController.addToCart)
+router.put('/add-to-cart', [verifyToken, cartValidation.addToCart], cartController.addToCart)
 
 export default router
