f8f2ea22bd9b8bd2d76e0e5925f038bdb5519132
Nguyễn Kim Quốc Nam <namnkq2603@gmail.com>
2024-08-08 15:58:55 +0700

    fix(feat/update-status-order): update role use to update status must be admin role

--- server/src/routes/v1/orderRoute.ts
+++ server/src/routes/v1/orderRoute.ts
@@ -1,11 +1,16 @@
 import express, { Router } from 'express'
 import orderController from '~/controllers/orderController'
 import { verifyToken } from '~/middlewares/jwtMiddleware'
+import { isAdmin } from '~/middlewares/permissionMiddleware'
 import orderValidation from '~/validations/orderValidation'
 
 const router: Router = express.Router()
 
 router.post('/', [verifyToken, orderValidation.createOrder], orderController.createOrder)
-router.put('/:orderId/status', [verifyToken, orderValidation.updateOrderStatus], orderController.updateOrderStatus)
+router.put(
+  '/:orderId/status',
+  [verifyToken, isAdmin, orderValidation.updateOrderStatus],
+  orderController.updateOrderStatus
+)
 
 export default router
